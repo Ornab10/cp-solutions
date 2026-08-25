bb8af5d59c4a31ec96d036af52c7dc1b4ce123bd
Al Israki Ornab <ornabalisraki@gmail.com>
2026-08-26 02:08:35 +0600

    734A

---
+++ CodeForces/734A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int countA = 0;
+    int countD = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'A')
+            countA++;
+        else
+            countD++;
+    }
+
+    if (countA > countD)
+        cout << "Anton" << endl;
+    else if (countA < countD)
+        cout << "Danik" << endl;
+    else
+        cout << "Friendship" << endl;
+
+    return 0;
+}
